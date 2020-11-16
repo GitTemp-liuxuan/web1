63656bc42f2a2d5b015932ea488482d2701f7594
Gittemp-liuxuan <1723673466@qq.com>
2020-11-16 08:51:10 +0800

    上课编码

---
+++ 第十三节课/js初步学习.js/jslearning.js
@@ -0,0 +1,11 @@
+
+
+function orange(){
+    var array = [123,15,45,85,96,78,1,13,41]
+    for (let index=0;index < array.length; index++){
+        const element = array[index];
+        console.log(element);
+    }
+}
+
+orange();
